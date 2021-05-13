8d10ba68d924e735150569c0fab529cd6609f322
Randy Rodriguez <Randyr2125@msn.com>
2021-05-12 21:32:48 -0400

    initial commit, app routes work.

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+# user = User.create(username: "Randyr2125", email:"Randyr2125@test.com")
+tweet = Tweet.create!(user_id:1, text:"This is my 2nd tweet")
